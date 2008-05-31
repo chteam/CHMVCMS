f30204a770fa6e72caa2efa38706f45a2d0136d5
chsword_cp <SND\chsword_cp>
2008-05-31 01:49:19 +0000

    控制器扩展

---
+++ Src/CHCMS/YouXiArticle/Controllers/Extension/RenderExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Web.Mvc;
+
+namespace YouXiArticle.Controllers
+{
+	static public class RenderExtension
+	{
+		static public void RenderText(this Controller c,string str){
+			c.HttpContext.Response.Write(str);
+		}
+	}
+}
