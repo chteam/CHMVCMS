42d9f9db5786325d5b709b5e7b4fb4285f80be82
chsword_cp <SND\chsword_cp>
2008-05-31 01:39:27 +0000

    通用于文本与URL之间的类

---
+++ Src/CHCMS/YouXiArticle/Entity/CommonArticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YouXiArticle
+{
+	public class CommonArticle
+	{
+		public long ID { get; set; }
+		public string Title
+		{
+			get;
+			set;
+		}
+		public string Body { set; get; }
+		public string Author { set; get; }
+		public bool IsUrl { set; get; }
+		public DateTime Addtime { get; set; }
+		public byte Type { get; set; }
+	}
+}
