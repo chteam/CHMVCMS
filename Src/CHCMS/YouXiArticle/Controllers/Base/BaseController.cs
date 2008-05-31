be06bc4d075cb02f456f4ac522fbb8afd04fc646
chsword_cp <SND\chsword_cp>
2008-05-31 01:49:03 +0000

    Controller base class 控制器基类

---
+++ Src/CHCMS/YouXiArticle/Controllers/Base/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YouXiArticle.Controllers
+{
+	public class BaseController : DataController
+    {
+        string _SubDomain = null;
+
+        public string SubDomain
+        {
+            get
+            {
+                if (_SubDomain == null)
+                {
+                    _SubDomain = this.Request.Url.Host.Split('.')[0].ToLower();
+                }
+                return _SubDomain;
+            }
+        }
+    }
+}
---
+++ Src/CHCMS/YouXiArticle/Controllers/Base/DataController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YouXiArticle.Models;
+
+namespace YouXiArticle.Controllers
+{
+    public class DataController : Controller
+    {
+        #region DataContext DBC单例属性
+        ArticleDataContext _DBC = null;
+
+        public ArticleDataContext DataContext
+        {
+            get {
+                if (_DBC == null)
+                    _DBC = new ArticleDataContext();
+                return _DBC;
+            }
+        }
+		DBExt _dbExt = null;
+		public DBExt DBExt
+		{
+			get
+			{
+				if (_dbExt == null)
+				{
+					_dbExt = new DBExt(this.DataContext);
+				}
+				return _dbExt;
+			}
+		}
+        #endregion
+    }
+}
