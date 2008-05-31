678c20d6d4f60375266459d3a19ed9f0d269b494
chsword_cp <SND\chsword_cp>
2008-05-31 01:47:09 +0000

    Global files 全局文件

---
+++ Src/CHCMS/YouXiArticle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YouXiArticle.GlobalApplication" Language="C#" %>
---
+++ Src/CHCMS/YouXiArticle/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace YouXiArticle
+{
+    public class GlobalApplication : System.Web.HttpApplication
+    {
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			// Note: Change the URL to "{controller}.mvc/{action}/{id}" to enable
+			//       automatic support on IIS6 and IIS7 classic mode
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.MapRoute(
+				"Article",
+				"{NavType}/{id}.html",
+				new { action = "Article", controller = "home", NavType = NavType.Article }
+			);
+			routes.MapRoute(
+				"List",
+				"{NavType}/{id}/{everypage}/{page}.html",
+				new { action = "List", controller = "home", page = 1, everypage = 20, NavType = NavType.List }
+			);
+
+			routes.MapRoute(
+				"Mvc",
+				"{controller}/{action}/",
+				new { action = "Index" }
+			);
+
+			routes.MapRoute(
+				"Default",
+				"Default.aspx",
+				new { controller = "Home", action = "Index", id = "" }
+			);
+		}
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+			VelocityTools.Init();
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
