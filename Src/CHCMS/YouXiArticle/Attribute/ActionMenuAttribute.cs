b9e8729cd67868d2b2c0b3a44ab474b7e421eaee
chsword_cp <SND\chsword_cp>
2008-05-31 01:33:35 +0000

    签入Attribute,可以自动生成菜单

---
+++ Src/CHCMS/YouXiArticle/Attribute/ActionMenuAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YouXiArticle
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ActionMenuAttribute : ActionTypeAttribute
+    {
+        public ActionMenuAttribute() : base(ActionType.Menu) { }
+        public string Title { get; set; }
+    }
+}
---
+++ Src/CHCMS/YouXiArticle/Attribute/ActionTypeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YouXiArticle
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ActionTypeAttribute : Attribute
+    {
+        List<ActionType> _actionTypeList = new List<ActionType>();
+        public ActionTypeAttribute(params ActionType[] at)
+        {
+            foreach (ActionType a in at)
+            {
+                _actionTypeList.Add(a);
+            }
+        }
+    }
+}
