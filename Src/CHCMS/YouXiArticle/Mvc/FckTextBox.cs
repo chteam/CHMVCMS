8ac28fb2584e7f65965254c55a358c18c39d8257
chsword_cp <SND\chsword_cp>
2008-06-02 02:51:27 +0000

    将扩展改为系统命名空间 change ext method to namespace of system

--- Src/CHCMS/YouXiArticle/Mvc/FckTextBox.cs
+++ Src/CHCMS/YouXiArticle/Mvc/FckTextBox.cs
@@ -1,29 +1,48 @@
-﻿using System;
-using System.Data;
-using System.Configuration;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Xml.Linq;
-
-namespace System.Web.Mvc
+﻿namespace System.Web.Mvc
 {
+	using System;
+	using System.Globalization;
+	/// <summary>
+	/// Fckeditor的HTMLHelper
+	/// http://chsword.cnblogs.com/
+	/// </summary>
 	static public class FckTextBoxExt
 	{
+		/// <summary>
+		/// Fckeditor的HTMLHelper,可以与同名ViewData绑定
+		/// </summary>
+		/// <param name="u">HtmlHelper</param>
+		/// <param name="name">Html的NAME</param>
+		/// <returns></returns>
 		public static string FckTextBox(this HtmlHelper u, string name)
 		{
-			return u.FckTextBox(name, "");
+			return u.FckTextBox(name, null);
 		}
-		public static string FckTextBox(this HtmlHelper u, string name,object text){
-
-			return u.FckTextBox(name, text.ToString());
-		}
-		public static string FckTextBox(this HtmlHelper u, string name,string text)
+		/// <summary>
+		/// Fckeditor的HTMLHelper
+		/// </summary>
+		/// <param name="u"></param>
+		/// <param name="name">Html name </param>
+		/// <param name="value">内容</param>
+		/// <returns></returns>
+		public static string FckTextBox(this HtmlHelper u, string name, object value)
 		{
+			return u.FckTextBox(name, value.ToString());
+		}
+		/// <summary>
+		/// Fckeditor的HTMLHelper
+		/// </summary>
+		/// <param name="u"></param>
+		/// <param name="name">Html name</param>
+		/// <param name="value">内容</param>
+		/// <returns></returns>
+		public static string FckTextBox(this HtmlHelper u, string name, string value)
+		{
+			if (value == null)
+			{
+				value = Convert.ToString(u.ViewDataContainer.ViewData[name], CultureInfo.InvariantCulture);
+			}
+
 			return string.Format(@"<textarea name=""{0}"" id=""{0}"" rows=""50"" cols=""80"" style=""width:100%; height: 600px"">{1}</textarea>
 <script type=""text/javascript"">
 	var oFCKeditor = new FCKeditor('{0}') ;
@@ -31,7 +50,7 @@ namespace System.Web.Mvc
 oFCKeditor.Height=400;
 	oFCKeditor.ReplaceTextarea() ;
 </script>
-", name, text);
+", name, value);
 
 		}
 	}
